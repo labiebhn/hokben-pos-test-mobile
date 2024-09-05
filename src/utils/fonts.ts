b034717c3b57f199567adeb8e0aec817b5bffcfe
Labieb Hammam Nurri <labiebhn@gmail.com>
2024-09-05 15:23:33 +0700

    🔧 Setup palettes and fonts

---
+++ src/utils/fonts.ts
@@ -0,0 +1,48 @@
+const fonts = {
+  h1: {
+    fontSize: 36,
+    fontWeight: 'bold',
+  },
+  h2: {
+    fontSize: 27,
+    fontWeight: 'bold',
+  },
+  h3: {
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  h4: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  h5: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  h6: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  h7: {
+    fontSize: 12,
+    fontWeight: 'bold',
+  },
+  h8: {
+    fontSize: 10,
+    fontWeight: 'bold',
+  },
+  label: {
+    fontSize: 12,
+  },
+  p: {
+    fontSize: 12,
+  },
+  small: {
+    fontSize: 10,
+  },
+  placeholder: {
+    fontSize: 12,
+  },
+};
+
+export default fonts;
---
+++ src/utils/palettes.ts
@@ -0,0 +1,8 @@
+const palettes = {
+  primary: '#FFC300',
+  secondary: '#18c026',
+  text: '#333333',
+  background: '#ffffff',
+};
+
+export default palettes;
